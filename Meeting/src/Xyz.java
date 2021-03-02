8cd00b19cb050ea78ad65db24ab75f15dce9bc4f
arunkishoregupta <78836558+arunkishoregupta@users.noreply.github.com>
2021-03-02 17:16:43 +0530

    Create Xyz.java
    by arun

--- Meeting/src/Xyz.java
+++ Meeting/src/Xyz.java
@@ -4,7 +4,7 @@ public class Xyz {
 	public static void main(String[] args) {
 		System.out.println(123);
 		System.out.println("ARUN");
-		System.out.println("Anshul");
+		System.out.println("Anshul loves (.........)");
 		// TODO Auto-generated method stub
 
 	}
