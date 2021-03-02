1e1aebb4cd7f843616bee7b842ab2fc5231035e6
Anshul Kumar Shakya <75451020+alexyder@users.noreply.github.com>
2021-03-02 17:11:10 +0530

    Update Xyz.java

--- Meeting/src/Xyz.java
+++ Meeting/src/Xyz.java
@@ -2,9 +2,6 @@
 public class Xyz {
 
 	public static void main(String[] args) {
-		System.out.println(123);
-		// TODO Auto-generated method stub
-
+		System.out.println("Anshul kkumar Shakya);
 	}
-
 }
