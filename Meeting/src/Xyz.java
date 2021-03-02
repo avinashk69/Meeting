c66f81735166e097d7f5b72408156e83fbf46695
Anshul Kumar Shakya <75451020+alexyder@users.noreply.github.com>
2021-03-02 17:21:24 +0530

    Update Xyz.java

--- Meeting/src/Xyz.java
+++ Meeting/src/Xyz.java
@@ -5,6 +5,7 @@ public class Xyz {
 		System.out.println(123);
 		System.out.println("ARUN");
 		System.out.println("Anshul loves (PUBG)");
+		hello i am trupti singh rathore from kolhapur
 		// TODO Auto-generated method stub
 
 	}
