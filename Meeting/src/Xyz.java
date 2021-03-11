409fda19cd3336478dccf5b78962ffd774526141
anshul shakya <anshulshakya.as6@gmail.com>
2021-03-11 16:48:45 +0530

    The Beginning

--- Meeting/src/Xyz.java
+++ Meeting/src/Xyz.java
@@ -6,17 +6,7 @@ public class Xyz {
 		System.out.println("ARUN");
 		System.out.println("Anshul loves (PUBG)");
 		System.out.println("tipsy here");
-		hello i am trupti singh rathore from kolhapur
-			hello ......this is arun kishore gupta from varanasi...
-				hello this is anshul from mars
-			
-			not locked
-			
-			yeahhhhh.....file is not locked
-
-			hello
-			jjllllklklklsljbjaksfjba
-			oqfuoewf
+		System.out.println("hello i am trupti singh rathore from kolhapur");
 
 	}
 
